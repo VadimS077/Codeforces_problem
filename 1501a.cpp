df3c4067cc66d83c3edec456b36586af324ff9d5
VadimS077 <110056050+VadimS077@users.noreply.github.com>
2022-10-17 22:02:34 +0300

    Add files via upload

---
+++ 1501a.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+#include <vector>
+
+
+int main() {
+    int t = 0;
+    std::cin >> t;
+    while (t--) {
+        int n = 0;
+        std::cin >> n;
+        std::vector <int> a;
+
+        for (int i = 0; i < n; i++) {
+            int ai = 0;
+            std::cin >> ai;
+            a.push_back(ai);
+
+
+
+        }int rost = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            if (a[i] == 1 && a[i - 1] == 1) {
+                rost += 5;
+            }
+            else {
+                if (a[i] == 1) {
+                    rost += 1;
+                }
+
+            }if (a[i] == 0 && a[i - 1] == 0 && i != 0) {
+                rost = -1;
+                break;
+            }
+        }std::cout << rost << std::endl;
+
+
+    }
+}
